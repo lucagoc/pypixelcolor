0e23594cd9ddeb2c2da58d625aae85edbc09879d
lucagoc <77584602+lucagoc@users.noreply.github.com>
2025-12-10 22:34:35 +0100

    Split send_text to multiple files

--- src/pypixelcolor/commands/__init__.py
+++ src/pypixelcolor/commands/__init__.py
@@ -30,21 +30,3 @@ COMMANDS = {
     "set_power": set_power.set_power,
     "show_slot": show_slot.show_slot,
 }
-
-# 0x80  -> ?
-# 1     -> ?
-COMMANDS_ID = {
-    (1, 0x80): set_time.set_time,
-    (3, 0x80): clear.clear,
-    (4, 0x80): set_brightness.set_brightness,
-    (6, 0x80): set_orientation.set_orientation,
-    # ---
-    (2, 1): delete.delete,
-    (4, 1): set_fun_mode.set_fun_mode,
-    (5, 1): set_fun_mode.set_pixel,
-    (6, 1): set_clock_mode.set_clock_mode,
-    (7, 1): set_power.set_power,
-    # ---
-    (0, 2): set_rhythm_mode.set_rhythm_mode_2,
-    (1, 2): set_rhythm_mode.set_rhythm_mode,
-}
